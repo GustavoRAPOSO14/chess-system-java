4753cb888536b280c26945371e5f9c333cc76730
Gustavo <gustavo.raposo@fatec.sp.gov.br>
2023-12-18 16:35:09 -0300

    Implementing possible moves of King

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,6 +30,7 @@ public class ChessMatch {
 	
 	public boolean[][] possibleMoves(ChessPosition sourcePosition){
 		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
 		return board.piece(position).possibleMoves();
 	}
 	
